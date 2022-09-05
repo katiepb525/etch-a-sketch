9c380ea1365175fd67591f72b98a207c26d938bb
katie <katiepb525@gmail.com>
2022-09-05 18:16:36 -0500

    fix bug where square hasn't been accepted by appendChild.

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const grid = document.querySelector('.grid-container')
 // create enough squares for a 16^2 grid using a loop
 for (let i = 0; i < 256; i++) {
     // variable for single square div
-    const square = document.createElement('div').style;
+    const square = document.createElement('div');
     // style width and height for a square
-    square.width = '10px';
-    square.height = '10px';
+    square.style.width = '10px';
+    square.style.height = '10px';
     // assign number id to square
     square.id = `square-container-${i}`;
     // add square to grid container
