7b7df0ed7263cfa8335699bf0660d970f4428d43
katie <katiepb525@gmail.com>
2022-09-05 18:00:22 -0500

    add selector for grid container

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// select "grid-container" in html
+const grid = document.querySelector('.grid-container')
+
 // create enough squares for a 16^2 grid using a loop
 for (let i = 0; i < 256; i++) {
     // variable for single square div
