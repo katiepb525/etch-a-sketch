f6eaccab6e7741b460dd4cbad63f697d0c9dafdb
katie <katiepb525@gmail.com>
2022-09-05 17:55:57 -0500

    add width/height to square

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // create enough squares for a 16^2 grid using a loop
 for (let i = 0; i < 256; i++) {
     // variable for single square div
-    const square = document.createElement('div');
-
+    const square = document.createElement('div').style;
+    // style width and height for a square
+    square.width = '10px';
+    square.height = '10px';
 
 }
