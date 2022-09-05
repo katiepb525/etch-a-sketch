c9d1928db97305c5d4f3200c2ae3192fc7853e77
katie <katiepb525@gmail.com>
2022-09-05 17:12:13 -0500

    create loop that loops 256 (16^2) times

---
+++ script.js
@@ -0,0 +1,7 @@
+// create enough squares for a 16^2 grid using a loop
+for (let i = 0; i < 256; i++) {
+    // variable for single square div
+    const square = document.createElement('div');
+
+
+}
