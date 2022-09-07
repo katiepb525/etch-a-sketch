c3b261f80a17316874cc53c4cc9135be7cce0cb9
katie <katiepb525@gmail.com>
2022-09-07 17:47:14 -0500

    check if entered pixel amount is valid through function

--- script.js
+++ script.js
@@ -5,16 +5,34 @@ const grid = document.querySelector('.grid-container');
 // select "grid-change" button in html
 const changeGrid = document.querySelector('.grid-change');
 
-//check if pixel amount is valid
-let pixelIsValid = false;
+
+// get a valid pixel amount
+function getValidPixel() {
+    // check if pixel amount is valid
+    let pixelIsValid = false;
+
+    // loop while pixel amount is invalid
+    while (pixelIsValid == false) {
+        pixelAmount = prompt("enter a new pixel amount per side!");
+
+        if (pixelAmount > 100 || pixelAmount < 5) {
+            alert("number out of bounds! between 5 and 100. please try again..")
+            pixelIsValid = false;
+
+        }
+        else if (pixelAmount <= 100 && pixelAmount >= 5) {
+            pixelIsValid = true;
+        }
+
+        if (pixelIsValid == true) {
+            break;
+        }
+    }
+}
 
 // add click listener for changeGrid (work in progress)
 changeGrid.addEventListener("click", () => {
-    pixelAmount = prompt("enter a new pixel amount per side!");
-    if (pixelAmount > 100 || pixelAmount < 5) {
-        alert("number out of bounds! between 5 and 100. please try again..");
-        pixelAmount = prompt("enter a new pixel amount per side!");
-    }
+    getValidPixel();
 }
 );
 
