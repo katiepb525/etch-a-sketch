8fb77f3c9f70d8b2fba13980fdeca65b417cb064
katie <katiepb525@gmail.com>
2022-09-07 17:50:35 -0500

    move creating grid action into its own function

--- script.js
+++ script.js
@@ -30,29 +30,34 @@ function getValidPixel() {
     }
 }
 
+function createGrid(pixelAmount) {
+    // create enough squares for a (pixelAmount)^2 grid using a loop
+    for (let i = 0; i < (Math.pow(pixelAmount, 2)); i++) {
+        // variable for single square div
+        const square = document.createElement('div');
+        // add a class of "square"
+        square.classList.add('square');
+
+        // add event listener for when square is hovered over, sets color to black
+        square.addEventListener("mouseover", () => square.setAttribute("style", "background-color: black"))
+        // assign number id to square
+        square.id = `square-container-${i}`;
+        // add square to grid container
+        grid.appendChild(square);
+    }
+
+}
+
 // add click listener for changeGrid (work in progress)
 changeGrid.addEventListener("click", () => {
     getValidPixel();
 }
 );
 
+createGrid(pixelAmount);
 
 // make grid-template-columns/row pixelAmount to begin with
 grid.style.gridTemplateColumns = `repeat(${pixelAmount}, 30px)`
 grid.style.gridTemplateRows = `repeat(${pixelAmount}, 30px)`
 
-// create enough squares for a 16^2 grid using a loop
-for (let i = 0; i < (Math.pow(pixelAmount, 2)); i++) {
-    // variable for single square div
-    const square = document.createElement('div');
-    // add a class of "square"
-    square.classList.add('square');
-
-    // add event listener for when square is hovered over, sets color to black
-    square.addEventListener("mouseover", () => square.setAttribute("style", "background-color: black"))
-    // assign number id to square
-    square.id = `square-container-${i}`;
-    // add square to grid container
-    grid.appendChild(square);
-}
 
