a5b6e7f7aae4e60b77ef9c6a94c21e9c4a167e16
katie <katiepb525@gmail.com>
2022-09-08 18:02:24 -0500

    split functions and organize

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ const grid = document.querySelector('.grid-container');
 // select "grid-change" button in html
 const changeGridBtn = document.querySelector('.grid-change');
 
+function changeGrid(pixelAmount) {
+    // make grid-template-columns/row pixelAmount
+    grid.style.gridTemplateColumns = `repeat(${pixelAmount}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${pixelAmount}, 1fr)`
+
+}
+
+// create enough squares for a (pixelAmount)^2 grid using a loop
+function createGrid(pixelAmount) {
+    for (let i = 0; i < (Math.pow(pixelAmount, 2)); i++) {
+        // variable for single square div
+        const square = document.createElement('div');
+        // add a class of "square"
+        square.classList.add('square');
+        // add event listener for when square is hovered over, sets color to black
+        square.addEventListener("mouseover", () => square.setAttribute("style", "background-color: black"))
+        // assign number id to square
+        square.id = `square-container-${i}`;
+        // add square to grid container
+        grid.appendChild(square);
+    }
+}
+
+window.onload = function () {
+
+    changeGrid(pixelAmount);
+    createGrid(pixelAmount);
+
+}
 
 // get a valid pixel amount
 function getValidPixel() {
@@ -16,11 +45,12 @@ function getValidPixel() {
         pixelAmount = prompt("enter a new pixel amount per side!");
 
         if (pixelAmount > 100 || pixelAmount < 5) {
-            alert("number out of bounds! between 5 and 100. please try again..")
+            alert("number out of bounds! between 5 and 100. please try again..");
             pixelIsValid = false;
 
         }
         else if (pixelAmount <= 100 && pixelAmount >= 5) {
+            changeGrid();
             pixelIsValid = true;
         }
 
